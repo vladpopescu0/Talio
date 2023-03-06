b33caee45fe9ace8160a96f923ad03af684cf3dc
Ioan Leolea <I.Leolea@student.tudelft.nl>
2023-03-06 10:43:43 +0100

    Add card and list classes

---
+++ commons/src/main/java/commons/CardList.java
@@ -0,0 +1,54 @@
+package commons;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import javax.persistence.*;
+import java.util.List;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
+@Entity
+public class CardList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String name;
+    @ElementCollection
+    private List<Card> cards;
+
+    public CardList() {
+
+    }
+
+    public CardList(String name, List<Card> cards){
+        this.name = name;
+        this.cards = cards;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+    }
+
+
+
+}
