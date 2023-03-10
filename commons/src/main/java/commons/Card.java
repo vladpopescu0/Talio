08d5a8aec385eb0396feab9c8e163b6c494154ef
Shashwat <S.Sahay-1@student.tudelft.nl>
2023-03-10 17:49:38 +0100

    Fixes pipeline

--- commons/src/main/java/commons/Card.java
+++ commons/src/main/java/commons/Card.java
@@ -4,8 +4,7 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
