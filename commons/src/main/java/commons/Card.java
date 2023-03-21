926487f22d97d02d792e044bbbc8092c38dd549a
Vlad Popescu <popescu-9@student.tudelft.nl>
2023-03-21 19:25:04 +0100

    Added delete card functionality and merged with the multiple server features and changed card with jsonIgnore

--- commons/src/main/java/commons/Card.java
+++ commons/src/main/java/commons/Card.java
@@ -1,5 +1,6 @@
 package commons;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -18,6 +19,7 @@ public class Card implements Serializable {
     private String name;
 
     @Transient
+    @JsonIgnore
     private CardList parentCardList;
 
     /**
